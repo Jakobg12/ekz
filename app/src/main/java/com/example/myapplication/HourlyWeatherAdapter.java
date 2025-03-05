b60733988283efdd947c92028b8a4f91d05e394f
darya <student-a502@PC-A502-12>
2025-03-05 09:47:43 +0500

    реализованы классы

---
+++ app/src/main/java/com/example/myapplication/HourlyWeatherAdapter.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+public class HourlyWeatherAdapter extends RecyclerView.Adapter<HourlyWeatherAdapter.ViewHolder> {
+    private List<HourlyWeather> hourlyWeatherList;
+    public HourlyWeatherAdapter(List<HourlyWeather> hourlyWeatherList) {
+        this.hourlyWeatherList = hourlyWeatherList;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.weather_itm, parent, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        HourlyWeather hourlyWeather = hourlyWeatherList.get(position);
+        holder.timeTextView.setText(hourlyWeather.getTime());
+        holder.temperatureTextView.setText(hourlyWeather.getTemperature());
+
+        switch (hourlyWeather.getTemperature()) {
+            case "cloudy":
+                holder.weatherImageView.setImageResource(R.drawable.cloudy);
+                break;
+            case "sunny":
+                holder.weatherImageView.setImageResource(R.drawable.Sun);
+                break;
+            case "rainy":
+                holder.weatherImageView.setImageResource(R.drawable.Rain);
+                break;
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return hourlyWeatherList.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView timeTextView;
+        public TextView temperatureTextView;
+        public ImageView weatherImageView;
+        public ViewHolder(View view) {
+            super(view);
+            timeTextView = view.findViewById(R.id.hours);
+            temperatureTextView = view.findViewById(R.id.gradus);
+            weatherImageView = view.findViewById(R.id.weather_image);
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,173 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+    Weather weather = new Weather();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        GetWeather getWeather = new GetWeather();
+        getWeather.execute();
+    }
+    class GetWeather extends AsyncTask<Void, Void, Void>
+    {
+        String body;
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            Document d_doc = null;
+            try {
+                d_doc = Jsoup.connect("https://api.weather.yandex.ru/v2/forecast?lat=52.37125&lon=4.89388")
+                        .header("X-Yandex-API-Key", "9223b2cf-2f9e-4162-8cc9-0f4726b69fc5")
+                        .ignoreContentType(true).get();
+                body = d_doc.text();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            if (body != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(body);
+                    JSONArray forecasts = jsonObject.getJSONArray("forecasts");
+                    for (ArrayList<HourlyWeather> weatherList : weather.hourlyWeatherData) {
+                        weatherList.clear();
+                    }
+                    for (int i = 0; i < forecasts.length(); i++) {
+                        JSONObject dailyForecast = forecasts.getJSONObject(i);
+                        JSONArray hours = dailyForecast.getJSONArray("hours");
+                        int sumTemp = 0;
+                        int count = 0;
+
+                        for (int j = 0; j < hours.length(); j++) {
+                            JSONObject hour = hours.getJSONObject(j);
+                            int temp = hour.getInt("temp");
+                            sumTemp += temp;
+                            count++;
+
+                            String time = hour.getString("hour") + ":00";
+                            String condition = hour.getString("condition");
+                            weather.hourlyWeatherData.get(i).add(new HourlyWeather(time, temp + "°C"));
+                        }
+
+                        if (i == 1) {
+                            weather.mondayAvgTemp = (sumTemp / count) + "°C";
+                        } else if (i == 2) {
+                            weather.tuesdayAvgTemp = (sumTemp / count) + "°C";
+                        }
+                    }
+                    addweather();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject jsonObject = new JSONObject(body);
+                TextView textView = findViewById(R.id.textView);
+                TextView textView2 = findViewById(R.id.pog);
+
+                String tempValue = jsonObject.getJSONObject("fact").getString("temp") + "°C";
+                String condition = jsonObject.getJSONObject("fact").getString("condition");
+
+                textView.setText(tempValue);
+                textView2.setText(condition);
+
+                TextView mondayAvgTextView = findViewById(R.id.monday_avg_temp);
+                TextView tuesdayAvgTextView = findViewById(R.id.tuesday_avg_temp);
+
+                mondayAvgTextView.setText(weather.mondayAvgTemp);
+                tuesdayAvgTextView.setText(weather.tuesdayAvgTemp);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void setWeather(View view)
+    {
+        setContentView(R.layout.weatherhous);
+        addspiner();
+        addHourlyWeather(0);
+    }
+    public void addweather()
+    {
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.pog);
+        textView.setText(weather.temp);
+        textView2.setText(weather.could);
+
+
+        TextView mondayAvgTextView = findViewById(R.id.monday_avg_temp);
+        TextView tuesdayAvgTextView = findViewById(R.id.tuesday_avg_temp);
+
+        mondayAvgTextView.setText(weather.mondayAvgTemp);
+        tuesdayAvgTextView.setText(weather.tuesdayAvgTemp);
+    }
+    public void addspiner() {
+        String[] add = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, add);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                addHourlyWeather(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Spinner", "No item selected");
+            }
         });
     }
+
+    public void addHourlyWeather(int dayIndex) {
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        HourlyWeatherAdapter adapter = new HourlyWeatherAdapter(weather.hourlyWeatherData.get(dayIndex));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void Back(View view)
+    {
+        setContentView(R.layout.activity_main);
+        GetWeather getWeather = new GetWeather();
+        getWeather.execute();
+    }
+
 }
